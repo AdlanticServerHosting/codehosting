824dd8676ae932e9206e7dce4ee5567d972e1921
AdlanticServerHosting <106774947+AdlanticServerHosting@users.noreply.github.com>
2022-06-05 12:39:57 -0400

    Update photopcommandaddons.js

--- js/photopcommandaddons.js
+++ js/photopcommandaddons.js
@@ -3,12 +3,12 @@ console.log("Photop Command Addons V1 Loaded! Have fun!")
 const uptime = "0"
 
 function uptime() {
-	while (true) {
-  const uptime = uptime + "1"
-	client.editor().setDescription(uptime + " | Boosted by Photop Addons").save()
+  while (true) {
+    const uptime = uptime + "1"
+    client.editor().setDescription(uptime + " | Boosted by Photop Addons").save()
   }
 }
 
 function runuptime() {
-setInterval(uptime, 1000)
+  setInterval(uptime, 1000)
 }
